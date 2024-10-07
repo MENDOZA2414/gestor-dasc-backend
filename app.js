e37992641dc30696e0475e26c4a50083f4a06edc
José Miguel <jmendoza_21@alu.uabcs.mx>
2024-10-06 19:03:12 -0700

    Eliminar HMTL y Vistas

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors');
 
 // Importación de rutas
 const userRoutes = require('./routes/UserRoutes');
-const indexRouter = require('./routes/index');
 const studentRoutes = require('./routes/StudentRoutes');  
 const internalAssessorRoutes = require('./routes/InternalAssessorRoutes');  
 const externalAssessorRoutes = require('./routes/ExternalAssessorRoutes');  
@@ -17,19 +15,19 @@ const studentDocumentationRoutes = require('./routes/StudentDocumentationRoutes'
 
 const app = express();
 
-// Configuración de la vista y contenido estático
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Rutas para usuarios
-app.use('/user', userRoutes);
+// Ruta raíz simple para verificar que el servidor está en funcionamiento
+app.get('/', (req, res) => {
+  res.json({ message: 'API running successfully' });
+});
 
 // Uso de rutas actualizadas
-app.use('/', indexRouter);
+app.use('/user', userRoutes);
 app.use('/students', studentRoutes);  // Rutas para alumnos
 app.use('/internalAssessors', internalAssessorRoutes);  // Rutas para asesores internos
 app.use('/externalAssessors', externalAssessorRoutes);  // Rutas para asesores externos
@@ -38,24 +36,16 @@ app.use('/applications', studentApplicationRoutes);  // Rutas para postulaciones
 app.use('/practicePositions', practicePositionRoutes);  // Rutas para vacantes
 app.use('/studentDocumentation', studentDocumentationRoutes);  // Rutas para documentos de alumnos
 
-// Ruta para servir el archivo HTML desde la carpeta public
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'public', 'index.html');
-  console.log('Sirviendo archivo:', filePath); // Esto imprimirá la ruta que se está utilizando
-  res.sendFile(filePath);
-});
-
-
 // Manejo de errores
 app.use((req, res, next) => {
   next(createError(404));
 });
 
 app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.json({ message: err.message, error: err });
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
