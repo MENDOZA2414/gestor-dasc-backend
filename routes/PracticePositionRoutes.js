404db08358e8746c585ad03c4cd49200a5e6f234
MENDOZA2414 <jmendoza_21@alu.uabcs.mx>
2025-06-01 14:04:47 -0700

    Ordenar rutas Vacantes

--- routes/PracticePositionRoutes.js
+++ routes/PracticePositionRoutes.js
@@ -1,41 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
 const practicePositionController = require('../controllers/PracticePositionController');
 const authMiddleware = require('../middlewares/AuthMiddleware');
 const checkRole = require('../middlewares/CheckRole');
 const checkUserType = require('../middlewares/CheckUserType');
 const checkUserTypeOrRole = require('../middlewares/CheckUserTypeOrRole');
 
-// Ruta para obtener las vacantes de práctica por ID de entidad
-router.get(
-  '/entidad/:entidadID',
-  authMiddleware,
-  checkRole(['Admin', 'SuperAdmin']),
-  practicePositionController.getPositionsByCompanyID
-);
+// ──────── Rutas protegidas ────────
 
-// Obtener todas las vacantes prácticas
-router.get(
-  '/all/:page/:limit',
-  authMiddleware,
-  practicePositionController.getAllPositions
-);
-
-// Obtener vacantes por estatus
-router.get(
-  '/',
-  authMiddleware,
-  practicePositionController.getPositionsByStatus
-);
-
-// Ruta para obtener una vacante de práctica por ID
-router.get(
-  '/:id',
-  authMiddleware,
-  practicePositionController.getPositionByID
-);
-
-// Ruta para crear una nueva vacante de práctica
+// Crear una nueva vacante de práctica
 router.post(
   '/create',
   authMiddleware,
@@ -43,7 +17,43 @@ router.post(
   practicePositionController.createPosition
 );
 
-// Cambiar el estatus de aceptacióon o rechazo de una vacante
+// Obtener todas las vacantes (paginadas)
+router.get(
+  '/all/:page/:limit',
+  authMiddleware,
+  practicePositionController.getAllPositions
+);
+
+// Obtener vacantes filtradas por estatus
+router.get(
+  '/',
+  authMiddleware,
+  practicePositionController.getPositionsByStatus
+);
+
+// Obtener vacantes por ID de entidad receptora
+router.get(
+  '/entidad/:entidadID',
+  authMiddleware,
+  checkRole(['Admin', 'SuperAdmin']),
+  practicePositionController.getPositionsByCompanyID
+);
+
+// Obtener vacante por ID
+router.get(
+  '/:id',
+  authMiddleware,
+  practicePositionController.getPositionByID
+);
+
+// Actualizar parcialmente una vacante por ID
+router.patch(
+  '/:id',
+  authMiddleware,
+  practicePositionController.patchPositionController
+);
+
+// Cambiar estatus (aceptar, rechazar) de una vacante por ID
 router.patch(
   '/status/:id',
   authMiddleware,
@@ -51,14 +61,7 @@ router.patch(
   practicePositionController.patchPositionStatus
 );
 
-// Ruta para actualizar parcialmente una vacante
-router.patch(
-  '/:id',
-  authMiddleware,
-  practicePositionController.patchPositionController
-);
-
-// Eliminación lógica controlada de vacante y opcionalmente de sus postulaciones
+// Eliminación lógica controlada de vacante y sus postulaciones (opcional)
 router.delete(
   '/delete/:id',
   authMiddleware,
